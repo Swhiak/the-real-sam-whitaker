fd085d3ec38ef60d2701bdaff9a461843a8a447a
Swhiak <sam.whitaker13@gmail.com>
2020-05-01 22:20:42 -0500

    side bar nav

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './Components/Home';
 import Nav from './Components/Nav';
-import './App.css';
+import './App.scss';
+
+/* TODO
+  * get sass going
+  * install and use fontawesome
+*/
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Nav>
+        <div className="app-frame">
+          <Nav />
+          <div className="app-body">
           <Switch>
             <Route path='/' component={Home} />
           </Switch>
-        </Nav>
+          </div>
+        </div>
+        
       </Router>
-      
     </div>
   );
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,27 @@
 import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
     const [display, setDisplay] = useState(true);
 
     return (
-        <div className='nav-pane'>
-            <div onClick={() => setDisplay(!display)}>
-                {display ? '<' : '>'}
+        <div className='nav-body' style={styles.navBody(display)}>
+            <div className='nav-header'>
+                <div className='nav-bars' onClick={() => setDisplay(!display)}>
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
             </div>
         </div>
     )
+}
+
+const styles = {
+    navBody: (d) => ({
+        maxWidth: '16%',
+        minWidth: '4%',
+        width: d ? '16%' : '4%',
+    })
 }
 
 export default Nav;
